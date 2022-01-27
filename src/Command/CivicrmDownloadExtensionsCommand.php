05e4b43fa60d841124d67569d97a2d828ab7bfa8
Sunil Pawar <sunil@skvare.com>
2022-01-27 21:29:23 +0530

    correct command name

--- src/Command/CivicrmDownloadExtensionsCommand.php
+++ src/Command/CivicrmDownloadExtensionsCommand.php
@@ -10,7 +10,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Filesystem\Filesystem;
 
 /**
- * Command class for 'civicrm:download-extensions-new' command.
+ * Command class for 'civicrm:download-extensions' command.
  */
 class CivicrmDownloadExtensionsCommand extends BaseCommand {
 
@@ -20,7 +20,7 @@ class CivicrmDownloadExtensionsCommand extends BaseCommand {
   protected function configure() {
     parent::configure();
 
-    $this->setName('civicrm:download-extensions-new')
+    $this->setName('civicrm:download-extensions')
       ->setDescription('Download CiviCRM extensions defined in composer.json');
   }
 
