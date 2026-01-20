6403296aac0cd43eb27537543ac3ad8615e3b72d
Sunil Pawar <sunil@skvare.com>
2026-01-20 18:41:57 +0530

    WP cms civicrm asset sync

--- src/Handler.php
+++ src/Handler.php
@@ -171,11 +171,19 @@ class Handler {
     // No need to sync any file in WordPress.
     if (array_key_exists('cms_type', $extra['civicrm']) &&
       strtolower($extra['civicrm']['cms_type']) == 'wordpress') {
-      return;
+      if (!empty($extra['civicrm-asset']['path'])) {
+        $destination = $extra['civicrm-asset']['path'];
+        $destination = getcwd() . DIRECTORY_SEPARATOR . $destination;
+      }
+      else {
+        return;
+      }
+    }
+    else {
+      $destination = './web/libraries/civicrm';
     }
     $source = $this->getCivicrmCorePath();
-    $destination = './web/libraries/civicrm';
-    $this->output("<info>Syncing CiviCRM web assets to /web/libraries/civicrm...</info>");
+    $this->output("<info>Syncing CiviCRM web assets to " . $destination . "...</info>");
     $vendor_path = $this->composer->getConfig()->get('vendor-dir');
     $this->util->removeDirectoryRecursively("{$destination}/packages/kcfinder");
     if ($this->filesystem->exists("{$vendor_path}/civicrm/civicrm-packages")) {
@@ -188,7 +196,12 @@ class Handler {
     }
     $this->filesystem->copy("{$vendor_path}/civicrm/civicrm-extension-plugin/src/civicrm.config.php", "{$destination}{$setting_php_file}/civicrm.config.php");
     $this->filesystem->copy("{$source}/extension-compatibility.json", "{$destination}/core/extension-compatibility.json");
-    $this->filesystem->copy("{$vendor_path}/civicrm/civicrm-extension-plugin/src/settings_location.txt", "{$destination}{$setting_php_file}/settings_location.php");
+    if (strtolower($extra['civicrm']['cms_type']) == 'wordpress') {
+      $this->filesystem->copy("{$vendor_path}/civicrm/civicrm-extension-plugin/src/settings_location_wp.txt", "{$destination}{$setting_php_file}/settings_location.php");
+    }
+    else {
+      $this->filesystem->copy("{$vendor_path}/civicrm/civicrm-extension-plugin/src/settings_location.txt", "{$destination}{$setting_php_file}/settings_location.php");
+    }
     if ($this->filesystem->exists("{$source}/js/wysiwyg/ck-options.json")) {
       $this->filesystem->copy("{$source}/js/wysiwyg/ck-options.json", "{$destination}/core/js/wysiwyg/ck-options.json");
     }
